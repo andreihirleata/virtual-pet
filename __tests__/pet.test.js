7a2d24c0f46b90da642b5b0bb58f4ae4e4f1ecee
andreihirleata <hirleata.andrei@gmail.com>
2020-03-18 21:11:10 +0000

    added constructors for name / age , added method to add 1 to the pet's age

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,7 +1,17 @@
-const Pet = require('../src/pet');
+const Pet = require("../src/pet");
 
-describe('constructor', () => {
-    it('returns an object', () => {
-      expect(new Pet('Gojira')).toBeInstanceOf(Object);
-    });
+describe("constructor", () => {
+  const myPet = new Pet("Fido");
+  myPet.growUp();
+  myPet.growUp();
+  myPet.growUp();
+  it("returns an object", () => {
+    expect(new Pet("Gojira")).toBeInstanceOf(Object);
   });
+  it("instatiates an object", () => {
+    expect(myPet.name).toEqual("Fido");
+  });
+  describe("grow up method", () => { 
+    expect(myPet.age).toEqual(3);
+  }) 
+});
