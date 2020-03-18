47bbe4cda17ba442969b96b222a399dfec1e773e
andreihirleata <hirleata.andrei@gmail.com>
2020-03-18 21:09:22 +0000

    added constructors for name / age , added method to add 1 to the pet's age

--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,11 @@
-function Pet() {
-
+class Pet {
+    constructor(name, age = 0) {
+        this.name = name;
+        this.age = age;
+    }
+    growUp() {
+        this.age += 1;
+    }
 }
 
 module.exports = Pet;
