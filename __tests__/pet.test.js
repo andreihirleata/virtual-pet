6c0d9ebf2cd6b1d13218672df7d24be386208f5c
andreihirleata <hirleata.andrei@gmail.com>
2020-03-18 21:48:14 +0000

    added hunger and fitness stats, and a method to walk your pet in order to increase its fitness

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,17 +1,48 @@
+/* eslint-disable */
 const Pet = require("../src/pet");
 
+const firstPet = new Pet("Fido");
+firstPet.growUp();
+firstPet.growUp();
+firstPet.growUp();
+const secondPet = new Pet("Gojira", 2, 5, 7);
+secondPet.growUp();
+
 describe("constructor", () => {
-  const myPet = new Pet("Fido");
-  myPet.growUp();
-  myPet.growUp();
-  myPet.growUp();
   it("returns an object", () => {
     expect(new Pet("Gojira")).toBeInstanceOf(Object);
   });
   it("instatiates an object", () => {
-    expect(myPet.name).toEqual("Fido");
+    expect(firstPet.name).toEqual("Fido");
   });
-  describe("grow up method", () => { 
-    expect(myPet.age).toEqual(3);
-  }) 
+});
+
+describe("grow up method", () => {
+  it("expects age to increase by one upon growing up", () => {
+    expect(firstPet.age).toEqual(3);
+  });
+});
+
+describe("hunger test", () => {
+  it("increases hunger by 5 on growing up", () => {
+    expect(firstPet.hunger).toEqual(15);
+    expect(secondPet.hunger).toEqual(10);
+  });
+});
+
+describe("fitness test", () => {
+  it("expects fitness to decrease by 3 upon growing up", () => {
+    expect(firstPet.fitness).toEqual(1);
+    expect(secondPet.fitness).toEqual(4);
+  });
+});
+
+describe("walk test", () => {
+  it("expects fitness to increase by 4 upon walking up to a max of 10", () => {
+    firstPet.walk();
+    expect(firstPet.fitness).toEqual(5);
+    secondPet.walk();
+    secondPet.walk();
+    expect(secondPet.fitness).toEqual(10);
+  });
 });
--- src/pet.js
+++ src/pet.js
@@ -1,10 +1,27 @@
+const AGE_INCREMENT = 1;
+const HUNGER_INCREMENT = 5;
+const FITNESS_REDUCTION = 3;
+const FITNESS_INCREMENT = 4;
+const MAX_FITNESS = 10;
 class Pet {
-    constructor(name, age = 0) {
+    
+    constructor(name, age = 0 , hunger = 0, fitness = 10) {
         this.name = name;
         this.age = age;
+        this.hunger = hunger;
+        this.fitness = fitness;
     }
     growUp() {
-        this.age += 1;
+        this.age += AGE_INCREMENT;
+        this.hunger += HUNGER_INCREMENT;
+        this.fitness -= FITNESS_REDUCTION;
+    }
+
+    walk() {
+        if ((this.fitness + 4) >= 10) {
+            this.fitness = 10;
+        }
+       else this.fitness += FITNESS_INCREMENT;
     }
 }
 
